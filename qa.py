28e511d0b2bfda8f8673f1376191decc039ec2ba
Levi <levgoolie@gmail.com>
2025-07-04 02:04:08 +1000

    Update qa.py

--- qa.py
+++ qa.py
@@ -1,21 +1,21 @@
 import os 
 
-BLACK ="\033[0;30m"#line:7
-RED ="\033[0;31m"#line:8
-GREEN ="\033[0;32m"#line:9
-BROWN ="\033[0;33m"#line:10
-BLUE ="\033[0;34m"#line:11
-PURPLE ="\033[0;35m"#line:12
-CYAN ="\033[0;36m"#line:13
-LIGHT_GRAY ="\033[0;37m"#line:14
-DARK_GRAY ="\033[1;30m"#line:15
-LIGHT_RED ="\033[1;31m"#line:16
-LIGHT_GREEN ="\033[1;32m"#line:17
-YELLOW ="\033[1;33m"#line:18
-LIGHT_BLUE ="\033[1;34m"#line:19
-LIGHT_PURPLE ="\033[1;35m"#line:20
-LIGHT_CYAN ="\033[1;36m"#line:21
-LIGHT_WHITE ="\033[1;37m"#line:22
+BLACK ="\033[0;30m"
+RED ="\033[0;31m"
+GREEN ="\033[0;32m"
+BROWN ="\033[0;33m"
+BLUE ="\033[0;34m"
+PURPLE ="\033[0;35m"
+CYAN ="\033[0;36m"
+LIGHT_GRAY ="\033[0;37m"
+DARK_GRAY ="\033[1;30m"
+LIGHT_RED ="\033[1;31m"
+LIGHT_GREEN ="\033[1;32m"
+YELLOW ="\033[1;33m"
+LIGHT_BLUE ="\033[1;34m"
+LIGHT_PURPLE ="\033[1;35m"
+LIGHT_CYAN ="\033[1;36m"
+LIGHT_WHITE ="\033[1;37m"
 
 while True:
  def banner():
